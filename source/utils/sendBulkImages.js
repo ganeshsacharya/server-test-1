f0c3f867167ba4927515ea4a26425d848ef617e8
Pranav-n-js <80742552+Pranav-n-js@users.noreply.github.com>
2021-08-26 15:42:34 +0530

    Update sendBulkImages.js

--- source/utils/sendBulkImages.js
+++ source/utils/sendBulkImages.js
@@ -6,13 +6,13 @@ const checkEligibility = (async(dogBreed)=>{
         let subBreedData = await axios.get("https://dog.ceo/api/breed/"+dogBreed+"/list")
         if(subBreedData.data.message.length>1){
             let dogBreedImageData = await axios.get("https://dog.ceo/api/breed/"+dogBreed+"/images")
-            dogBreedImage=await dogBreedImageData.data.message
+            dogBreedImage= dogBreedImageData.data.message
         }
     } catch (error) {
         dogBreedImage.push(error.response.data.message);
     }
     
     
-    return await dogBreedImage
+    return dogBreedImage
 })
 module.exports = checkEligibility
