e0f713e23f43d310129dacc81e3881a937f12a6f
Pranav-n-js <80742552+Pranav-n-js@users.noreply.github.com>
2021-08-26 11:16:08 +0530

    tasks completed

--- source/app.js
+++ source/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const hbs = require('hbs')
 const path = require('path')
+
 const dogBreed = require('./utils/dogBreed')
+const subBreed = require('./utils/subBreed')
+const dogImages = require('./utils/dogImages')
+const bulkImages = require('./utils/sendBulkImages')
 
 const app =  express()
 const staticPath = path.join(__dirname,'../public')//creating the static path for browser JS
@@ -17,13 +21,78 @@ app.get("",(req,res)=>{
 app.get('/dogBreed',(req,res)=>{
     dogBreed((error, dogBreedList)=>{
         if(error){
+            res.statusCode=404
+            res.setHeader("content-type","application/json")
             return res.send({success:false,error})
         }
         else{
+            res.statusCode=200
+            res.setHeader("content-type","application/json")
             return res.send({success:true,dogBreedList})
         }
     })        
 })
+app.get('/subBreed',(req,res)=>{
+    subBreed(req.query.dogBreed,(error, subBreedList)=>{
+        if(error){
+            res.statusCode=404
+            res.setHeader("content-type","application/json")
+            return res.send({success:false,error})
+        }
+        else{
+            res.statusCode=200
+            res.setHeader("content-type","application/json")
+            return res.send({success:true,subBreedList})
+        }
+    })        
+})
+app.get('/pagination',(req,res)=>{
+    const page= parseInt(req.query.page)
+    const dogBreed= req.query.dogBreed
+    const subBreed= req.query.subBreed
+    dogImages(dogBreed,subBreed,(error, dogImageList)=>{
+        if(error){
+            res.statusCode=404
+            res.setHeader("content-type","application/json")
+            return res.send({success:false,error})
+        }
+        else{
+            const startIndex=(page-1)*5
+            const endIndex=page*5
+            
+            let resultingImages= dogImageList.slice(startIndex,endIndex)
+            if(resultingImages.length==0){
+                resultingImages="No more Images to show"
+            }
+            res.statusCode=200
+            console.log(resultingImages);
+            res.setHeader("content-type","application/json")
+            return res.send({success:true,dogImages:resultingImages})
+        }
+        
+    }) 
+})
+
+app.post('/getImagesByBreed/bulk', async(req,res)=>{
+    let dogBreedInput = req.query.breed
+    if(typeof(dogBreedInput)==="string"){
+        dogBreedInput=[dogBreedInput]
+    }
+    let images={}
+    let imageData=[]
+    let dogBreedName=[]
+    dogBreedInput.forEach(async (dogBreed)=>{
+            dogBreedName.push(dogBreed)
+            imageData.push(bulkImages(dogBreed))
+        })
+        let dogBreedImageData= await Promise.all(imageData)
+        console.log(dogBreedImageData.length);
+        for(let index=0;index<dogBreedImageData.length;index++){
+            images[dogBreedName[index]]=dogBreedImageData[index]
+        }
+    return res.send(images)
+})
+
 app.get('*',  (req,res)=>{
     res.send('404',{errorMessage:"404.. Requested page didn't found"})
 })
