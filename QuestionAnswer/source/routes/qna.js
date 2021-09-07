f76c1ff7b458a09fe6bf53a0538b79330f220d55
Pranav-n-js <80742552+Pranav-n-js@users.noreply.github.com>
2021-09-07 10:34:28 +0530

    correcting post data

--- QuestionAnswer/source/routes/qna.js
+++ QuestionAnswer/source/routes/qna.js
@@ -1,54 +1,34 @@
-const express = require("express")
-const { Op } = require("sequelize");
-const {answer,sequelize,question,User  ,previousQuestion}= require("D:/Node/inventory/models")
-const router= express.Router()
-const sendQuestion = require("../utils/sendQuestion")
 
-router.route("/send").get(async(req,res)=>{
-    let response=""
-    try {
-        if(req.query){
-            const userID= req.query.userID
-            response=await sendQuestion(userID)
-            return res.json(response) 
+const express = require("express")
+const {sequelize, User, inventory}= require("D:/Node/inventory/models")
+const router= express.Router()
+
+router.route("/add").post(async(req,res)=>{
+    if(req.body){
+        try{
+            const {userName, coin, life}= req.body
+            const users= await User.create({userName})
+            const invent= await inventory.create({coin, life, UserUserID:users.user_ID})
+            return res.status(200).send({users:users,invent})
+            
+        }catch(err){
+            console.log(err);
+            return res.status(404).send({error:err})
         }
+    }else return res.status(400).send({error:"some error"})
+})
+
+router.route("/findByID").get(async(req,res)=>{
+    try {
+        const findUser= await User.findOne({
+            include: [inventory],
+             where: {user_ID: req.query.ID}
+        })
+        return res.status(200).json({findUser})
     } catch (error) {
         console.log(error);
-        res.status(404).json(error)
+        return res.status(404).json(error)
     }
 })
-router.route("/checkAnswer").get(async(req,res)=>{
-    if(req.query){
-        let message=""
-        let response={}
-        let nextQuestion
-        try {
-            const {questionID, userID, option}= req.query
-            const questionData = await question.findOne({
-                attribute:['questionID', "answerId"], 
-                where:{questionID:questionID},
-                include:answer
-            })  
-            if(option==questionData.answer.option){
-                message="Correct Answer. Next Question is"
-                nextQuestion=await sendQuestion(userID)
-            }else{
-                message="Sorry Wrong answer.. Please Try again"
-                let  userData = await User.findOne({
-                    attribute:['currentLevel'],
-                    where:{
-                        user_ID:userID
-                    }
-                })
-                userData.currentLevel=1;
-                userData.save()
-            }
-            response={message,nextQuestion}
-            return res.json(response)
-        } catch (error) {
-            console.log(error);
-            return res.status(400).json({error:"some problem"})
-        }
-    }
-})
+
 module.exports= router
